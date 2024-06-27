57d786e93b6a8762a785d4d7917831501a4e0af5
delmy21lk <delmy.lira48@gmail.com>
2024-06-26 22:26:35 -0600

    Initial commit

---
+++ src/test/java/com/delmylira48/LiterAlura_Challenge/LiterAluraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delmylira48.LiterAlura_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
